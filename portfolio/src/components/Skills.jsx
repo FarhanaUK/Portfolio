0327e5b37912cac7012b5b0881e744379e468ece
Farhana Aktar <farhanaaktar@live.co.uk>
2024-12-30 12:52:08 +0000

    top gradient added

--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -25,11 +25,14 @@ function Skills() {
   return (
     <div className="p-8">
       <h1 className="text-3xl text-white font-bold mb-8">Skills</h1>
-      <div className="flex justify-center space-x-10 flex animate-scroll">
+      <div className="flex justify-center space-x-10 flex">
         {skills.map((skill, index) => (
           <div 
           key={index}
-          className="flex flex-col items-center border-t rounded-lg border border-[#1f223c] bg-[#11152c]  w-28 h-28 flex justify-center ">
+          className="relative flex flex-col items-center border-t rounded-lg border border-[#3c1f23] bg-[#2c1127]  w-28 h-28 flex justify-center ">
+
+<div className="absolute top-0 left-0 w-full h-[1px] bg-gradient-to-r from-transparent via-pink-600 to-transparent"></div>
+
             <div className="mb-2 ">
               <img 
               src={skill.image}
