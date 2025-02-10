8e10ace8a37e31c9b05f8ef2234fa15f2ee4475b
Farhana Aktar <farhanaaktar@live.co.uk>
2025-02-10 12:01:18 +0000

    gradient border style added

--- portfolio/src/pages/About.jsx
+++ portfolio/src/pages/About.jsx
@@ -3,14 +3,15 @@ import Lottie from "lottie-react";
 
 function About() {
   return (
-    <div className="mb-6 text-lg leading-relaxed flex justify-center md:flex-row text-white p-12 mx-4 mt-28">
-      <section className="md:w-3/2 space-y-6">
-        <div className="flex flex-col items-center border-2 border-solid rounded-lg bg-slate-900 p-6">
-          <h1 className="flex justify-center text-2xl leading-relaxed mb-4 bg-cyan-500 w-1/3 rounded-lg px-4">
-            My Journey into Web Development
+    <div className="mb-6 text-lg leading-relaxed flex justify-center md:flex-row text-white p-6 md:p-12 mx-4 mt-28">
+      <section className="md:w-2/3 space-y-6">
+        <div className="flex flex-col items-center border border-indigo-900 rounded-lg bg-slate-900 p-6 relative">
+        <div className="absolute top-0 left-0 w-full h-[1px] bg-gradient-to-r from-transparent via-cyan-500 to-transparent"></div>
+          <h1 className="flex justify-center text-2xl leading-relaxed mb-4 bg-indigo-900 w-full md:w-2/3 rounded-lg">
+            My Journey Into Web Development
           </h1>
           <p>
-            My journey into web development started with a simple question:{" "}
+            My journey ianto web development started with a simple question:{" "}
             <span className="italic">"How do websites do that?" </span>
             This curiosity sparked a deep dive into both front-end and back-end
             technologies, leading me to hands-on projects and real-world
@@ -20,125 +21,175 @@ function About() {
           </p>
 
           <br />
-          <div className="flex w-full md:flex-row items-start justify-between">
-    
- 
-    <div className="w-full md:w-1/2">
-      <ul className="list-disc pl-6 text-lg">
-        <li>JavaScript</li>
-        <li>React</li>
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>Tailwind CSS</li>
-      </ul>
-      <p className="my-6">
-        These technologies allow me to build dynamic, user-friendly
-        applications that address real-world challenges and provide
-        seamless, interactive experiences.
-      </p>
-    </div>
+          <div className="flex flex-col md:flex-row w-full items-start justify-between">
+            <div className="w-full md:w-1/2">
+              <ul className="list-disc pl-6 text-lg">
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>Tailwind CSS</li>
+              </ul>
+              <p className="my-6">
+                These technologies allow me to build dynamic, user-friendly
+                applications that address real-world challenges and provide
+                seamless, interactive experiences.
+              </p>
+            </div>
 
-    
-    <div className="w-full md:w-1/2 flex justify-end items-start mt-[-70px] mb-[-70px]">
-      <Lottie
-        animationData={laptop}
-        style={{ width: "100%", maxWidth: "500px", height: "auto", maxHeight: "450px" }}
-      />
-    </div>
+            <div className="w-full md:w-1/2 flex justify-end items-start md:mt-[-70px] md:mb-[-70px]">
+              <Lottie
+                animationData={laptop}
+                style={{
+                  width: "100%",
+                  maxWidth: "500px",
+                  height: "auto",
+                  maxHeight: "450px",
+                }}
+              />
+            </div>
+          </div>
+        </div>
 
-  </div>
-</div>
-
-        <div className="border-2 border-solid rounded-lg p-6">
-          <h1 className="text-2xl leading-relaxed">Professional Experience</h1>
+        <div className="flex flex-col items-center border border-indigo-900 rounded-lg bg-slate-900 p-6 relative">
+        <div className="absolute top-0 left-0 w-full h-[1px] bg-gradient-to-r from-transparent via-cyan-500 to-transparent"></div>
+          <h1 className="flex justify-center text-2xl leading-relaxed mb-4 bg-indigo-900 w-full md:w-2/3 rounded-lg">
+            Professional Experience
+          </h1>
           <p>
             Before becoming a developer, I worked in customer service and as a
             trainer at Amazon, where I honed skills like:
           </p>
 
           <br />
+          <div className="flex flex-col md:flex-row w-full items-start justify-between">
+            <div className="w-full md:w-1/2">
+              <ul className="list-disc pl-6 text-lg">
+                <li>
+                  Communication: Effectively conveying complex ideas to diverse
+                  audiences.
+                </li>
+                <li>
+                  Empathy: Understanding user pain points to develop better
+                  solutions.
+                </li>
+                <li>
+                  Conflict Resolution: Navigating challenges with creative
+                  problem-solving.
+                </li>
+              </ul>
+              <p className="my-6">
+                These experiences are invaluable in my development work, where I
+                prioritize user needs and accessibility.
+              </p>
+            </div>
 
-          <ul className="list-disc pl-6 mb-6 text-lg">
-            <li>
-              Communication: Effectively conveying complex ideas to diverse
-              audiences.
-            </li>
-            <li>
-              Empathy: Understanding user pain points to develop better
-              solutions.
-            </li>
-            <li>
-              Conflict Resolution: Navigating challenges with creative
-              problem-solving.
-            </li>
-          </ul>
-          <div className="flex justify-center items-center md:w-1/2 mt-8 md:mt-0">
-            <Lottie
-              animationData={laptop}
-              style={{ width: "100%", maxWidth: "500px", height: "auto" }}
-            />
+            <div className="w-full md:w-1/2 flex justify-end items-start md:mt-[-70px] md:mb-[-70px]">
+              <Lottie
+                animationData={laptop}
+                style={{
+                  width: "100%",
+                  maxWidth: "500px",
+                  height: "auto",
+                  maxHeight: "450px",
+                }}
+              />
+            </div>
           </div>
-
-          <p>
-            These experiences are invaluable in my development work, where I
-            prioritize user needs and accessibility.
-          </p>
         </div>
-        <div className="border-2 border-solid rounded-lg p-6">
-          <h1 className="text-2xl leading-relaxed">Outside of Coding</h1>
+
+        <div className="flex flex-col items-center border border-indigo-900 rounded-lg bg-slate-900 p-6 relative">
+        <div className="absolute top-0 left-0 w-full h-[1px] bg-gradient-to-r from-transparent via-cyan-500 to-transparent"></div>
+          <h1 className="flex justify-center text-2xl leading-relaxed mb-4 bg-indigo-900 w-full md:w-2/3 rounded-lg">
+            Outside of Coding
+          </h1>
           <p>
-            When I’m not coding, I’m focused on personal growth and staying
+          When I’m not coding, I’m focused on personal growth and staying
             active. I’m passionate about:
+
           </p>
 
           <br />
+          <div className="flex flex-col md:flex-row w-full items-start justify-between">
+            <div className="w-full md:w-1/2">
+              <ul className="list-disc pl-6 text-lg">
+                <li>
+                  Personal Development: Always looking for ways to grow both
+                  professionally and personally.
+                </li>
+                <li>Fitness: Pilates keeps me energized and focused</li>
+                <li>
+                  Traveling: Exploring new cultures fuels my creativity and
+                  broadens my perspective.
+                </li>
+              </ul>
+            </div>
 
-          <ul className="list-disc pl-6 mb-6 text-lg">
-            <li>
-              Personal Development: Always looking for ways to grow both
-              professionally and personally.
-            </li>
-            <li>Fitness: Pilates keeps me energized and focused</li>
-            <li>
-              Traveling: Exploring new cultures fuels my creativity and broadens
-              my perspective.
-            </li>
-          </ul>
-          <div className="flex justify-center items-center md:w-1/2 mt-8 md:mt-0">
-            <Lottie
-              animationData={laptop}
-              style={{ width: "100%", maxWidth: "500px", height: "auto" }}
-            />
+            <div className="w-full md:w-1/2 flex justify-end items-start md:mt-[-70px] md:mb-[-70px]">
+              <Lottie
+                animationData={laptop}
+                style={{
+                  width: "100%",
+                  maxWidth: "500px",
+                  height: "auto",
+                  maxHeight: "450px",
+                }}
+              />
+            </div>
           </div>
         </div>
 
-        <div className="border-2 border-solid rounded-lg p-6">
-          <h1 className="text-2xl leading-relaxed">My Philosophy</h1>
+        <div className="flex flex-col items-center border border-indigo-900 rounded-lg bg-slate-900 p-6 relative">
+        <div className="absolute top-0 left-0 w-full h-[1px] bg-gradient-to-r from-transparent via-cyan-500 to-transparent"></div>
+        <h1 className="flex justify-center text-2xl leading-relaxed mb-4 bg-indigo-900 w-full md:w-2/3 rounded-lg">
+            My Philosophy
+          </h1>
           <p>
             I’m driven by the challenge of solving complex problems with clean,
             efficient solutions. Whether it’s designing an intuitive user
             interface or building a powerful back-end, I always aim to deliver
             applications that not only work well but also delight users.
           </p>
-          <div className="flex justify-center items-center md:w-1/2 mt-8 md:mt-0">
-            <Lottie
-              animationData={laptop}
-              style={{ width: "100%", maxWidth: "500px", height: "auto" }}
-            />
+
+          <br />
+          <div className="flex w-full md:flex-row items-start justify-between">
+          <div className="w-full md:w-1/2 flex justify-end items-start md:mt-[-70px] md:mb-[-70px]">
+              <Lottie
+                animationData={laptop}
+                style={{
+                  width: "100%",
+                  maxWidth: "500px",
+                  height: "auto",
+                  maxHeight: "450px",
+                }}
+              />
+            </div>
           </div>
         </div>
 
-        <div className="border-2 border-solid rounded-lg p-6">
-          <h1 className="text-2xl leading-relaxed">Let’s Connect!</h1>
+        <div className="flex flex-col items-center border border-indigo-900 rounded-lg bg-slate-900 p-6 relative">
+        <div className="absolute top-0 left-0 w-full h-[1px] bg-gradient-to-r from-transparent via-cyan-500 to-transparent"></div>
+        <h1 className="flex justify-center text-2xl leading-relaxed mb-4 bg-indigo-900 w-full md:w-2/3 rounded-lg">
+            Let’s Connect!
+          </h1>
           <p>
             Thank you for checking out my portfolio. If you’re interested in
             collaborating or exploring my projects, feel free to get in touch!
           </p>
-          <div className="flex justify-center items-center md:w-1/2 mt-8 md:mt-0">
-            <Lottie
-              animationData={laptop}
-              style={{ width: "100%", maxWidth: "500px", height: "auto" }}
-            />
+
+          <br />
+          <div className="flex w-full md:flex-row items-start justify-between">
+          <div className="w-full md:w-1/2 flex justify-end items-start md:mt-[-70px] md:mb-[-70px]">
+              <Lottie
+                animationData={laptop}
+                style={{
+                  width: "100%",
+                  maxWidth: "500px",
+                  height: "auto",
+                  maxHeight: "450px",
+                }}
+              />
+            </div>
           </div>
         </div>
       </section>
