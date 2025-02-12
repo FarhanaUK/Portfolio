27dfff23573fc1c96fac0ca78a975cbdd94ee468
Farhana Aktar <farhanaaktar@live.co.uk>
2025-02-12 14:56:16 +0000

    Bacground changes

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -10,6 +10,7 @@ import Projects from './pages/Projects'
 import Skills from './pages/Skills'
 import 'animate.css'
 import background2 from '../src/assets/background2.json'
+import bg2 from '../src/assets/bg2.json'
 import Lottie from 'lottie-react'
 import { Routes, Route, useLocation} from 'react-router-dom'
 
@@ -21,7 +22,7 @@ const App = () => {
     <div className="bg-neutral-900 min-h-screen font-mono flex flex-col ">
        <div className="absolute top-0 left-0 w-full h-[1px] bg-gradient-to-r from-transparent via-cyan-500 to-transparent"></div>
        <div className="absolute left-0 top-0 z-0">
-       {!isAboutMe && (<Lottie animationData={background2} style={{ width: '40%', height: '30%' }} />)}
+       {!isAboutMe && (<Lottie animationData={bg2} style={{ width: '120%'}} />)}
       </div>
       <Header />
         <main className='flex-grow'>
